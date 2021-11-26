a9a3148d638fe72fea99f5c2b3bad191f64c31b3
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-26 15:49:15 -0500

    Adding Logger and configuring socket port

--- src/main/java/com/bahrath/springboot/rsocket/controller/RSocketPatientController.java
+++ src/main/java/com/bahrath/springboot/rsocket/controller/RSocketPatientController.java
@@ -2,15 +2,21 @@ package com.bahrath.springboot.rsocket.controller;
 
 import com.bahrath.springboot.rsocket.model.ClinicalPatientData;
 import com.bahrath.springboot.rsocket.model.Patient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import reactor.core.publisher.Mono;
 
 @Controller
 public class RSocketPatientController {
 
+    Logger logger = LoggerFactory.getLogger(RSocketPatientController.class);
+
     @MessageMapping("get-patient-data")
-    public Mono<ClinicalPatientData> requestResponse(Patient patient) {
+    public Mono<ClinicalPatientData> requestResponse(@RequestBody Patient patient) {
+        logger.info("Received Patient: "+patient);
         return Mono.just(new ClinicalPatientData(90, "80/120"));
     }
 
